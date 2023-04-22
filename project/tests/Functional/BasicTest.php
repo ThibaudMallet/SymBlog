23fefe73537bec3eaa041acc6bbdfbcfedf4acd1
thibaud <thibaud.mallet@hotmail.fr>
2023-04-22 06:50:10 +0200

    configure test environment

---
+++ project/tests/Functional/BasicTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BasicTest extends WebTestCase
+{
+    public function testEnvironmentIsOk(): void
+    {
+        $client = static::createClient();
+
+        $client->request(Request::METHOD_GET ,'/');
+
+        $this->assertResponseIsSuccessful();
+    }
+}
